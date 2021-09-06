9f5ad82ac36b064e3d9279571eff6ba690b92d5d
Liang34 <51255027+Liang34@users.noreply.github.com>
2021-09-06 10:11:33 +0800

    userManage

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,14 @@ const routes = [
         component: () => import('@/views/Welcome.vue')
       },
       {
+        name: 'user',
+        path: '/system/user',
+        meta: {
+          title: '用户管理'
+        },
+        component: () => import('@/views/User.vue')
+      },
+      {
         name: 'approve',
         path: '/system/approve',
         meta: {
